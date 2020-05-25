f5f766e6f5f8f3d00e3b6f4660e0c23066e9aff7
wAnto97 <antonino.geraci5@gmail.com>
2020-05-25 13:08:22 +0200

    Working_net

--- Modified_AlexNet.py
+++ Modified_AlexNet.py
@@ -19,10 +19,10 @@ class ReverseLayerF(Function):
         return output, None
 
 class DANN_AlexNet(nn.Module):
-    def __init__(self,num_classes = 100,pretrained=False):
-        super(nn.Module,self).__init__()
+    def __init__(self,num_classes = 1000,pretrained=False):
+        super(DANN_AlexNet,self).__init__()
 
-        self.features_exractor = nn.Sequential(
+        self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -38,7 +38,7 @@ class DANN_AlexNet(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
-        self.class_classifier = nn.Sequential(
+        self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
@@ -59,26 +59,17 @@ class DANN_AlexNet(nn.Module):
         )
 
     def forward(self, x, alpha=None):
-        features_extractor = self.features_exractor
+        features = self.features_exractor
         # Flatten the features:
-        features_extractor = features_extractor.view(features_extractor.size(0), -1)
+        features = features.view(features.size(0), -1)
         # If we pass alpha, we can assume we are training the discriminator
         if alpha is not None:
             # gradient reversal layer (backward gradients will be reversed)
-            reverse_feature = ReverseLayerF.apply(features_extractor, alpha)
+            reverse_feature = ReverseLayerF.apply(features, alpha)
             discriminator_output = self.domain_classifier(reverse_feature(x))
             return discriminator_output
         # If we don't pass alpha, we assume we are training with supervision
         else:
             # do something else
-            class_outputs = self.class_classifier(self.features_exractor(x))
+            class_outputs = self.classifier(self.features_exractor(x))
             return class_outputs
-
-def dann_alexnet(pretrained=False,num_classes=100):
-    model = DANN_AlexNet(num_classes)
-
-    if pretrained:
-        state_dict = load_state_dict_from_url('https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',progress=progress,strict = False)
-        model.load_state_dict(state_dict)
-
-    return model
